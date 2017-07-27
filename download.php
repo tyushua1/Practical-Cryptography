a24633181ba2e32ee9e05e3a2ed462b09ee4cef3
ghan3 <1031749562@qq.com>
2017-07-28 02:44:29 +0800

    文件下载

--- download.php
+++ download.php
@@ -14,79 +14,67 @@ function display(){
         if($filename==$resultArray[$i]){
           //$returnArray[$num]=$resultArray[$i];
           //$num++;
-          echo "$filename";
-
-
+          //echo "$filename";
           //echo "<script><a href='./upload/1.txt',Downloads='file1'>llalala</a></script>";
-          /*echo "<a href='./upload/";
+          echo "<a href='./upload/";
           echo $filename;
           echo "'>";
           echo $filename;
           echo "</a>";
           echo "</br>";
-*/?>
-          <!DOCTYPE html>
-          <html>
-              <head>
-                  <meta charset="utf-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                  <title></title>
 
-                  <link href="http://apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
-
-                  <link href="http://cdn.bootcss.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-
-                  <script src="http://apps.bdimg.com/libs/jquery/2.0.0/jquery.min.js"></script>
-
-                  <script src="http://apps.bdimg.com/libs/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-
-
-                  <style type="text/css">
-
-                       body{background: url() no-repeat;background-size:cover;font-size: 16px;}
-                       .form{background: rgba(255,255,255,0.3);width:500px;margin:100px auto;white-space:nowrap;}
-
-
-          .fa{display: inline-block;top: 27px;left: 6px;position: relative;color:#ccc;}
-                      #login_form{display: block;}
-                      #register_form{display: none;}
-
-                      input[type="text"],input[type="text"]{padding-left:26px;}
-                      <?php
-                      echo $filename;
-                      echo "'>";
-                      echo $filename;
-                      echo "</a>";
-                      echo "</br>";
-                      ?>
-
-                  </style>
-              </head>
-              <body>
-              <div class="container">
-          <div class="form row">
-                      <form class="form-horizontal col-sm-offset-3 col-md-offset-3"
-           id="login_form" method="post" action="sharelink.php" >
-
-
-                      </form>
-                  </div>
-             </body>
-          </html>
-
-
-<?php
         }
       }
     }
   }
-  //echo "$num";
-  //for($i=0;$i<$num;$i++){
-    //echo $returnArray[$i];
-  closedir($handler);
-  //return $returnArray;
+            //echo "$num";
+            //for($i=0;$i<$num;$i++){
+              //echo $returnArray[$i];
+    closedir($handler);
+            //return $returnArray;
 }
 
-
-display();
 ?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+
+        <link href="http://apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="http://cdn.bootcss.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+
+        <script src="http://apps.bdimg.com/libs/jquery/2.0.0/jquery.min.js"></script>
+
+        <script src="http://apps.bdimg.com/libs/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+
+
+        <style type="text/css">
+
+             body{background: url() no-repeat;background-size:cover;font-size: 16px;}
+             .form{background: rgba(255,255,255,0.3);width:500px;margin:100px auto;white-space:nowrap;}
+
+
+.fa{display: inline-block;top: 27px;left: 6px;position: relative;color:#ccc;}
+            #login_form{display: block;}
+            #register_form{display: none;}
+
+            input[type="text"],input[type="text"]{padding-left:26px;}
+
+        </style>
+    </head>
+    <body>
+    <div class="container">
+<div class="form row">
+            <form class="form-horizontal col-sm-offset-3 col-md-offset-3"
+ id="login_form" method="post" action="sharelink.php" >
+            <?php  display();  ?>
+
+            </form>
+        </div>
+   </body>
+</html>
